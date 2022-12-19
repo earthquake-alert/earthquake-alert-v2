ca2d36a27b885f2e95f3dc3eeb7b8e36ad2511f5
Yuto Watanabe <yuto.w51942@gmail.com>
2022-12-19 23:14:42 +0900

    修正

--- db/sql/001_schema.sql
+++ db/sql/001_schema.sql
@@ -35,9 +35,11 @@ CREATE TABLE IF NOT EXISTS `Earthquakes` (
 # 津波情報と地震情報をつなげるやつ
 # 津波1に対して地震多or無があるので
 CREATE TABLE IF NOT EXISTS `Tsunamis` (
+    `id` INT UNSIGNED AUTO_INCREMENT NOT NULL,
     `tsunami_id` INT NOT NULL,
     `event_id` INT NOT NULL,
-    `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
 );
 
 # 津波情報
